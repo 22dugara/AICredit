4c4bfb1bcbdd8089920ad1ebf997e14e4b767ab0
22dugara <22dugara@gmail.com>
2019-05-30 20:19:03 -0400

    final

--- project/signin/views.py
+++ project/signin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 import json
+
 # Create your views here.
 
 def signinView(request):
@@ -11,5 +12,5 @@ def manifestView(request):
 	with open('static/json/manifest.json') as f:
 		data = json.load(f)
 		print(data)
-	return JsonResponse(data)
+	return HttpResponse(json.dumps(data), content_type="application/json")
 	
